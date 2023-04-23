129c48ce56f5b91e65c8b62e4e29484141f04e7c
Amit <a.u.aua937@gmail.com>
2023-04-23 17:39:21 -0400

    Hero section
    Added hero section, but adrian had added a min-height
    for some reason which was screwing up my whole content and briniging it down to centere
    so had to fix the css as well.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
+import { logo } from "../assets";
+
 const Hero = () => {
-  return <header></header>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="Image_Generator" className="w-28 object-contain" />
+        <button
+          type="button"
+          className="black_btn"
+          onClick={() => {
+            window.open(
+              "https://github.com/amitupreti/image-generator-with-react-and-ARDIC-Image-Synthesizer",
+              "_blank"
+            );
+          }}
+        >
+          GitHub
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Image Generator with
+        <span className="orange_gradient">
+          {" "}
+          <br className="max-md:hidden" />
+          ArdicAI
+        </span>
+      </h1>
+      <h2 className="desc">
+        Create breathtaking images with the help of AI. Just enter the text and
+        click on the generate button and let the AI do the rest. violla!
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
